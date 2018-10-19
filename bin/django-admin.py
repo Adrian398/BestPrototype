5c1cba77da7fce9ebfa513f65e70bef3b705aa99
Sametros <adrian@stellaris.de>
2018-10-19 15:13:43 +0200

    start the fucking project!

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/stellaris/anaconda3/envs/prototype/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ prototype_version_1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PrototypeVersion1Config(AppConfig):
+    name = 'prototype_version_1'
